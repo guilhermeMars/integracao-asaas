8f89c2fd00e80eae010de5660bbc78e6124f81b2
guilhermeMars <guilhermemspiandorin@gmail.com>
2024-09-30 11:37:35 -0300

    feat: Adicionada paginação no consumo da API

--- cobrancas-usuarios.py
+++ cobrancas-usuarios.py
@@ -12,57 +12,82 @@ headers = {
     "access_token": token_ebramev
 }
 
-response = requests.get(url_cobranca, headers=headers)
+cobranca_data = []
 
-cobrancas = response.json()
-cobrancas_filtred = [
-    {
-        "Identificador": item["id"],
-        "Data de criacao": item["dateCreated"],
-        "Usuario": item["customer"],
-        "Valor": item["value"],
-        "Valor original": item["originalValue"],
-        "Descricao": item["description"],
-        "Tipo de cobranca": item["billingType"],
-        "Situacao": item["status"],
-        "Vencimento": item["dueDate"],
-        "Vencimento original": item["originalDueDate"],
-        "Data de Pagamento": item["paymentDate"],
-        "Data de credito": item["creditDate"],
-        "Forma de pagamento": item["billingType"],
-        "Data estimada de Credito": item["estimatedCreditDate"],
-        **({"Data de confirmacao": item["confirmedDate"]} if "confirmedDate" in item else {}),
-        "Valor Liquido": item["netValue"],
-        "Numero da fatura": item["invoiceNumber"]
+pag = 0
+limit = 10
+while True :
 
-    } for item in cobrancas['data']
-]
+    url_pag = f"{url_cobranca}?offset={pag}?limit={limit}"
+    temp_req = requests.get(url_pag, headers=headers)
+    temp_data = temp_req.json()
 
-response = requests.get(url_usuarios, headers=headers)
+    cobrancas_filtred = [
+        {
+            "Identificador": item["id"],
+            "Data de criacao": item["dateCreated"],
+            "Usuario": item["customer"],
+            "Valor": item["value"],
+            "Valor original": item["originalValue"],
+            "Descricao": item["description"],
+            "Tipo de cobranca": item["billingType"],
+            "Situacao": item["status"],
+            "Vencimento": item["dueDate"],
+            "Vencimento original": item["originalDueDate"],
+            "Data de Pagamento": item["paymentDate"],
+            "Data de credito": item["creditDate"],
+            "Forma de pagamento": item["billingType"],
+            "Data estimada de Credito": item["estimatedCreditDate"],
+            **({"Data de confirmacao": item["confirmedDate"]} if "confirmedDate" in item else {}),
+            "Valor Liquido": item["netValue"],
+            "Numero da fatura": item["invoiceNumber"]
 
-usuarios = response.json()
-usuarios_filtred = [
-    {
-        "Identificador": item["id"],
-        "Nome": item["name"],
-        "Email": item["email"],
-        "Celular": item["mobilePhone"],
-        "Fone": item["phone"],
-        "Cpf ou CNPJ": item["cpfCnpj"],
-        "Emails adicionais": item["additionalEmails"]
-    } for item in usuarios['data']
-]
+        } for item in temp_data['data']
+    ]
+
+    if temp_data['hasMore'] == False:
+        break
+
+    cobranca_data.append(cobrancas_filtred)
+
+    pag+= limit
+    print(f"Rodou {pag}")
+
+usuarios_data = []
+
+pag = 0
+limit = 10
+while True :
+
+    url_pag = f"{url_usuarios}?offset={pag}?limit={limit}"
+    temp_req = requests.get(url_pag, headers=headers)
+    temp_data = temp_req.json()
+
+    usuarios_filtred = [
+        {
+            "Identificador": item["id"],
+            "Nome": item["name"],
+            "Email": item["email"],
+            "Celular": item["mobilePhone"],
+            "Fone": item["phone"],
+            "Cpf ou CNPJ": item["cpfCnpj"],
+            "Emails adicionais": item["additionalEmails"]
+        } for item in temp_data['data']
+    ]
+
+    if temp_data['hasMore'] == False:
+        break
+
+    usuarios_data.append(usuarios_filtred)
+
+    pag+= limit
+    print(f"Rodou {pag}")
 
 complete_data = {
-    "Cobrancas": cobrancas_filtred,
-    "Usuarios": usuarios_filtred
+    "Cobrancas": cobranca_data,
+    "Usuarios": usuarios_data
 }
 
-indent_json = json.dumps(complete_data, indent=2);
-
-# print(json.dumps(cobrancas_filtred, indent=2))
-# print("\n")
-# print(json.dumps(usuarios_filtred, indent=2))
-print(indent_json)
-with open("cobrancas_usuarios.json", "w") as outfile:
-    outfile.write(indent_json)
+json_indent = json.dumps(complete_data, indent=2)
+with open("cobrancas_usuarios_3.json", "w") as outfile:
+    outfile.write(json_indent)
